c99f54f638e093398fe934bb4c045352fa94a66c
Cruz Wootten <78571191+LilWikipedia@users.noreply.github.com>
2024-11-18 22:48:05 -0800

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,24 @@
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { defineConfig } from 'vite';
+// import { defineConfig } from 'vite';
 
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const dirname = path.dirname(__filename);
 
-export default defineConfig({
-  base: "/rf-meter/",
-  server: {
-    host: "::",
-    port: 8080,
+  
+
+({
+    base: "/rf-meter/",
+    server: {
+      host: "::",
+      port: 8080,
   },
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(dirname, "./src"),
     },
   },
   build: {},
