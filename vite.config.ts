c13de4f6fd87c2197e50c78a989354412008c804
Cruz Wootten <78571191+LilWikipedia@users.noreply.github.com>
2024-11-18 22:38:02 -0800

    b

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-//import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
 
-export default ({
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
   base: "/rf-meter/",
   server: {
     host: "::",
