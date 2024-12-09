3149a2a78c027438e279464b5134f2fd4ce9a8b7
Cruz Wootten <78571191+LilWikipedia@users.noreply.github.com>
2024-12-09 06:23:49 +0000

    app config

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,30 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ErrorBoundary from "./components/ErrorBoundary";
 import Index from "./pages/Index";
 
 const queryClient = new QueryClient();
 
 const App = () => (
-  <ErrorBoundary>
+  
   <QueryClientProvider client={queryClient}>
+    
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Route>
+      
+      <BrowserRouter basename="/rf-meter">
+      <Routes>
           <Route path="/" element={<Index />} />
-        </Route>
+      </Routes>  
       </BrowserRouter>
+      
     </TooltipProvider>
+    
   </QueryClientProvider>
-  </ErrorBoundary>
+ 
 );
 
 export default App;
